471b399924caad4646258aa1a3412f496d79080d
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:25:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_7
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            string b;
+            Console.WriteLine("Введите трехзначное число:");
+            a = int.Parse(Console.ReadLine());
+            b = a.ToString();
+
+
+            char[] inputarray = b.ToCharArray();
+            Array.Reverse(inputarray);
+            string output = new string(inputarray);
+            Console.WriteLine(output);
+
+        }
+    } 
+}
+   
