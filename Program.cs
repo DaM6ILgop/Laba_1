b1040abe075d58f1fee624b2a914515c7079dde9
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:24:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, l;
+            Console.WriteLine("Введите четырехзначное число:");
+            a = int.Parse(Console.ReadLine());
+          
+                int w = a / 1000; 
+                int r = (a - w * 1000) / 100; 
+                int e = (a - w * 1000 - r * 100) / 10;
+                int u = (a - w * 1000 - r * 100 - e * 10); 
+                l = (w * r * e * u); 
+                Console.WriteLine($"Ответ:{l}"); 
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
