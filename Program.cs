93bef3f95bf2ab8b019891c137ccdb5f34f29ab4
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:28:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Первое_задание_из_личных
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Кинопродукция ");
+
+            Console.WriteLine("Фильмы:3==>");
+            string a1, a2, a3;
+            {
+
+                Console.WriteLine("Фильм_1");
+                a1 = Console.ReadLine();
+
+                Console.WriteLine("Фильм_2");
+                a2 = Console.ReadLine();
+
+                Console.WriteLine("Фильм_3");
+                a3 = Console.ReadLine();
+            }
+                
+            Console.WriteLine("РЕЖИССЕР:3==>");
+            string b1, b2, b3;
+            {
+
+                Console.WriteLine("Режиссер_Фильма_1");
+                b1 = Console.ReadLine();
+
+                Console.WriteLine("Режиссер_Фильма_2");
+                b2 = Console.ReadLine();
+
+                Console.WriteLine("Режиссер_Фильма_3");
+                b3 = Console.ReadLine();
+            }
+            Console.WriteLine("Год_Выпуска:3==>");
+            int c1, c2, c3;
+            {
+
+                Console.WriteLine("Год_выпуска_1");
+                c1 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Год_выпуска_2");
+                c2 = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Год_выпуска_3");
+                c3 = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Тип_Фильма:3==>");
+            char d1, d2, d3;
+            {
+
+                Console.WriteLine("Тип_1");
+                d1 = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Тип_2");
+                d2 = char.Parse(Console.ReadLine());
+
+                Console.WriteLine("Тип _3");
+                d3 = char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Кинопродукция");
+
+            Console.WriteLine($"\t\tФильм\t\t\tРежисер\t\t\tГодВыпуска\tТип");
+            Console.WriteLine($"\t\t{a1}\t\t{b1}\t\t{c1}\t\t{d1}");
+            Console.WriteLine($"\t\t{a2}\t\t{b2}\t\t{c2}\t\t{d2}"); 
+            Console.WriteLine($"\t\t{a3}\t\t{b3}\t\t{c3}\t\t{d3}");
+
+            Console.WriteLine("Перечисляемый тип: Д-Драма, К-Комедия, М-Мелодрама, Б-Боевик, А-Мультфильм");
+        }
+    }
+}
