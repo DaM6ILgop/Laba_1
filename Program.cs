ba03e93ac054a8ad61573440335ee7d9e6c6c1fc
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:18:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f, a;
+
+            Console.WriteLine("Введите дробное число (число с точкой ) ");
+            f = double.Parse(Console.ReadLine());
+            int c = (int)f;
+             a = (c - f) * 10;
+            a = (int)a;
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+
+           
+
+          
+
+
+
+
+
+
+        }
+    }
+}
