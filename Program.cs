f3bd01820b3a6665e94218a8b100162a51644ed5
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:19:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_из_лыбы_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, c, g;
+            Console.WriteLine("Введите прошедшее время в секунда");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a >= 0)
+            {
+                g = a / 3600;
+                int f = (int)g;
+                c = (a - (f * 3600)) / 60;
+                Console.WriteLine($"Прошло {g} часов и {c} минут");
+            }
+
+            else
+            {
+                Console.WriteLine("Ошибка");
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
