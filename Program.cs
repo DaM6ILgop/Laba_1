06465ebce6688b9f192d45ae5130f79ff3f311cc
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:21:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Программа_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Введите первое значение ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            b = int.Parse(Console.ReadLine());
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine($"Первое значение: {a}, Второе значение: {b}  ");
+            
+             
+
+
+
+        }
+
+
+    }
+
+    
+        
+            
+           
+
+
+
+
+
+
+}    
+
