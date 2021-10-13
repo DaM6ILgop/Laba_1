b126560771d7ecaa583c2507fa318bfe287a78a3
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:23:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, d;
+            double r;
+            Console.WriteLine("Введите длину первого катета ");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите длину второго катета ");
+            b = int.Parse(Console.ReadLine());
+            r = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2)); //Периметр 
+            d = (a * b) / 2; //Площадь
+            Console.WriteLine($"Периметр: {r}. Площадь: {d}") ;
+           
+
+
+
+
+
+
+
+        }
+    }
+}
