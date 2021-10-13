8d132efd82a223fb595efcf7745bf653d7e833ca
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:26:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, g;
+            Console.WriteLine("Введите число x ");
+            r = double.Parse(Console.ReadLine());
+            g = 3 * Math.Pow(r, 4) - 5 * Math.Pow(r, 3) + 2 * Math.Pow(r, 2) - r + 7;
+            Console.WriteLine(g);
+
+
+
+
+
+
+        }
+    }
+}
