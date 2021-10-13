67bcc2e9a50538dd488d4f5fe07ca8b8f81e4910
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-13 21:21:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Приложение_3
+
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Введите количество часов");
+           int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество минут");
+           int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите количество секунд");
+           int  c = Convert.ToInt32(Console.ReadLine());
+            int f = (a * 30);
+            int j = (b * 2);
+            double u = (c * 0.03);
+            double q = (f + j + u );
+            Console.WriteLine($"Угол между стрелками составляет {q} градусов" );
+
+
+        }
+    }
+}
