66bb77c1aacc48dfb4a74e9b053cd9b26a6a86ed
DaM6ILgop <91891112+DaM6ILgop@users.noreply.github.com>
2021-10-17 12:37:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Переделка_8_го_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, g;
+            Console.WriteLine("Введите число x ");
+            x = double.Parse(Console.ReadLine());
+            g = (((3 * x - 5) * x + 2) * x - 1) * x + 7;
+            Console.WriteLine($"Ваш ответ равен: {g} ");
+        }
+    }
+}
